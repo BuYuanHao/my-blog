70bc68663af27032a56b62ae1eef7e2adb8d3920
BuYuanHao <40720686+BuYuanHao@users.noreply.github.com>
2024-04-27 15:40:24 +0800

    help xiaopu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,23 +14,10 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">浦锐今天吃 {{ count }}个狗鸡</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      请点击帮帮小浦QAQ
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
